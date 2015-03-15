1d98ea16f8bdaf423776ac59b612b3a333a2b7d5
thetlk <jeremieboutoille@gmail.com>
2015-03-15 16:22:16 +0100

    update label displaying

--- tests/simple.asm
+++ tests/simple.asm
@@ -1,24 +1,22 @@
 	afc [$1], 1337
 	cop [$1], [$1]
-label-1:
+label_1:
 	cop [$2], [$0]
 	afc [$3], 10
 	inf [$2], [$2], [$3]
-	jmf [$2], label-2
 	cop [$2], [$0]
 	afc [$3], 5
 	equ [$2], [$2], [$3]
-	jmf [$2], label-3
 	cop [$2], [$1]
 	pri [$2]
-	jmp label-4
-label-3:
+	jmp label_4
+label_3:
 	cop [$2], [$0]
 	pri [$2]
-label-4:
+label_4:
 	cop [$2], [$0]
 	afc [$3], 1
 	add [$2], [$2], [$3]
 	cop [$0], [$2]
-	jmp label-1
-label-2:
+	jmp label_1
+label_2:
