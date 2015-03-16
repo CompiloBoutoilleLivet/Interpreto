1be8dd04a613172cc0fe9a95d0903a6f06394352
thetlk <jeremieboutoille@gmail.com>
2015-03-16 10:15:55 +0100

    remove warning

--- cpu.h
+++ cpu.h
@@ -10,6 +10,7 @@ struct cpu {
 };
 
 struct cpu *cpu_init(struct instr_manager *man);
+void cpu_reset(struct cpu *cpu);
 void cpu_run(struct cpu *cpu);
 void cpu_memdump(struct cpu *cpu);
 void cpu_exec_instr(struct cpu *cpu, struct instr *i);
